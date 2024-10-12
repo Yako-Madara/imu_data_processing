1382da8fdae8b67645d8287beba3e7a3396fca20
Yako-Madara <strelok.onaga@yandex.ru>
2024-10-13 01:38:47 +0300

    Загрузил небольшой пример из библиотеки PyPose

---
+++ lib/_virt_imu.py
@@ -0,0 +1,19 @@
+# модуль для создания класса виртуального imu
+
+class VirtualIMU:
+    def __init__(self) -> None:
+        ...
+        
+    def read_magnetometer(self):
+        ...
+        
+    def read_accelerometer(self):
+        ...
+    
+    def read_gyroscope(self):
+        ...
+    
+    def read_all_data(self):
+        ...  
+        
+        
